82f1b7d87db4ee33d3b81346f2ba929104c0d56b
BradHamm <bradham.opt@gmail.com>
2023-05-30 14:55:08 -0700

    Created a test README for reference and added fs functions within .then callback.

---
+++ README.md
@@ -0,0 +1,37 @@
+# The name
+
+## Description
+
+undefined
+
+## Installation
+
+The instructions
+
+## Usage
+
+The instructions for using
+
+## Table of Contents
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+
+## Contribution
+
+How to contribute
+
+## Tests
+
+Tests for the program
+
+## Questions?
+
+My Github: The github
+My email: The email
+The other questions info
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const licenseInformation() => {
+    fetch()
+}
+
 inquirer
   .prompt([
     {
       type: 'input',
       message: 'What is the name of your project?',
-      name: 'projectname',
+      name: 'projectName',
     },
     {
       type: 'input',
       message: 
       'Provide a brief description of the project. Here are some prompts to help guide you through creating your description.\n- What was your motivation behind the project?\n- Why did you build this project?\n- What problem does it solve?\n- What did you learn over the course of the project?\n',
-      name: 'descripton',
+      name: 'description',
     },
     {
       type: 'input',
@@ -20,25 +24,105 @@ inquirer
       name: 'install',
     },
     {
-      type: 'input',
-      message: 'Provide instructions and examples for use. This will comprise the "Usage" section of the READ ME: ',
-      name: 'usage',
+        type: 'input',
+        message: 'Provide your github so people can contact you with questions that they have about the project or the programs functionality: ',
+        name: 'questionsGithub',
+    },
+    {
+        type: 'input',
+        message: 'Provide your email so people can contact you with questions that they have about the project or the programs functionality: ',
+        name: 'questionsEmail',
+    },
+    {
+        type: 'input',
+        message: "If there are any additional methods of contact or stipulations regarding contact methods, provide them and it will be appended below your github. Type 'N' to disregard this section",
+        name: 'questions',
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for use. This will comprise the "Usage" section of the READ ME: ',
+        name: 'usage',
     },
     {
       type: 'input',
       message: 'Provide contribution guidelines which outline how users can contribute to the project. This will comprise the "Contribution Guidelines" section: ',
-      name: 'github',
+      name: 'contribution',
     },
     {
       type: 'input',
       message: 'Provide tests which showcase examples of your programs capabilities. This will comprise the "Tests" section: ',
-      name: 'github',
+      name: 'tests',
     },
     {
       type: 'list',
       message: 'Provide your github information:',
-      name: 'github',
+      name: 'license',
+      choices: licenseList,
+      editablelist: true,
     },
-    
   ])
-  .then((response) =>
+  .then((response) => {
+    fs.writeFile('README.md', `# ${response.projectName}\n\n`, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('README Project name imported - File successfully created.');
+      }
+    });
+
+    fs.appendFile('README.md', `## Description\n\n${response.description}\n\n`, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Description appended.');
+      }
+    });
+
+    fs.appendFile('README.md', `## Installation\n\n${response.install}\n\n`, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Installation appended.');
+      }
+    });
+
+    fs.appendFile('README.md', `## Table of Contents\n\nIf your README is long, add a table of contents to make it easy for users to find what they need.\n\n- [Installation](#installation)\n- [Usage](#usage)\n- [Contribution](#contribution)\n- [Tests](#tests)\n- [Questions?](#questions?)\n- [License](#license)\n\n`, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Table of contents appended.');
+      }
+    });
+
+    fs.appendFile('README.md', `## Usage\n\n${response.usage}\n\n`, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Usage instructions appended.');
+      }
+    });
+
+    fs.appendFile('README.md', `## Tests\n\n${response.tests}\n\n`, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Tests appended.');
+      }
+    });
+
+    fs.appendFile('README.md', `## Contribution\n\n${response.contribution}\n\n`, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Contribution methods appended.');
+      }
+    });
+
+    fs.appendFile('README.md', `## Questions?\n\nMy Github: ${response.questionsGithub}\nMy email: ${response.questionsEmail}\n${response.questions}`, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Questions section and social links appended.');
+      }
+    });
+  });
